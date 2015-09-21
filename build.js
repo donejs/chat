e5cc398338d7e8b9099a5158eab791bddaaa02cb
David Luecke <daff@neyeon.com>
2015-09-21 13:31:22 -0700

    Adding NW.js build

--- build.js
+++ build.js
@@ -2,6 +2,10 @@ var stealTools = require("steal-tools");
 
 var buildPromise = stealTools.build({
   config: __dirname + "/package.json!npm"
+}, {
+  bundleAssets: {
+    infer: false
+  }
 });
 
 var cordovaOptions = {
@@ -22,4 +26,29 @@ var buildCordova = process.argv.indexOf("cordova") > 0;
 
 if(buildCordova) {
   buildPromise.then(stealCordova.build).then(stealCordova.ios.emulate);
+}
+
+var nwOptions = {
+  buildDir: "./build",
+  platforms: ["osx"],
+  files: [
+    "package.json",
+    "app.html",
+
+    "node_modules/steal/steal.production.js",
+    "node_modules/place-my-order-assets/images/**/*"
+  ],
+  version: "0.12.3"
+};
+
+var stealNw = require("steal-nw");
+
+// Check if the cordova option is passed.
+var buildNW = process.argv.indexOf("nw") > 0;
+
+if(buildNW) {
+  buildPromise = buildPromise.then(function(buildResult){
+    console.log("NW")
+    stealNw(nwOptions, buildResult);
+  });
 }
