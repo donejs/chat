08ed91d2d86ca9b76dcff028017c4d7b7f652dcf
David Luecke <daff@neyeon.com>
2015-09-22 08:35:51 -0700

    Fix header

--- api/app.js
+++ api/app.js
@@ -11,7 +11,7 @@ app.configure(feathers.rest())
   .use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    res.header('Access-Control-Allow-Methods: DELETE, GET, HEAD, POST, PUT, OPTIONS, TRACE');
+    res.header('Access-Control-Allow-Methods', 'DELETE, GET, HEAD, POST, PUT, OPTIONS, TRACE');
     next();
   })
   .use(bodyParser.urlencoded({ extended: true }))
