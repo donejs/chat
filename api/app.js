9bf5f3b8ba5a8dd07f491a90796f91fbffa77faf
David Luecke <daff@neyeon.com>
2015-09-22 10:02:10 -0700

    API hardening

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 var feathers = require('feathers');
 var memory = require('feathers-memory');
+var hooks = require('feathers-hooks');
 var bodyParser = require('body-parser');
 
 var messages = memory('messages');
@@ -8,6 +9,7 @@ var app = feathers();
 // Add REST API support
 app.configure(feathers.rest())
   .configure(feathers.socketio())
+  .configure(hooks())
   .use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
@@ -17,5 +19,17 @@ app.configure(feathers.rest())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
   .use('/messages', messages);
+
+app.service('messages').before({
+  create: function(hook, next) {
+    var data = hook.data;
+    hook.data = {
+      name: data.name.substring(0, 50),
+      message: data.message.substring(0, 1000),
+      created_at: new Date()
+    };
+    next();
+  }
+});
 
 module.exports = app;
